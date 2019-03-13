557feb7767e74220e0c9d900942115282453df2b
alva <alva@10.0.0.8>
2019-03-13 22:30:41 +0100

    Apparat

---
+++ src/config/Apparat.java
@@ -0,0 +1,39 @@
+
+
+
+
+import java.sql.PreparedStatement;
+
+public class Apparat extends Connecting {
+	private PreparedStatement rs;
+	private String navn;
+	private String funksjon;
+	
+	public Apparat(String navn, String funksjon) {
+		this.navn = navn;
+		this.funksjon = funksjon;
+		registrerApparat();
+	}
+	
+	public void registrerApparat() {
+		connect();
+		try {
+			rs = conn.prepareStatement("INSERT INTO Apparat (Navn, Funksjon) VALUES ((?), (?))");
+			
+			rs.setString(1, this.navn);
+			rs.setString(2, this.funksjon);
+			rs.execute();
+			
+			conn.close();
+			
+		} catch (Exception e) { 
+            System.out.println("db error during prepare of insert into Apparat"+e);
+        }
+	}
+	
+	public static void main(String[] args) {
+		Apparat a1 = new Apparat("Romaskin", "Ro");
+		
+	}
+
+}
