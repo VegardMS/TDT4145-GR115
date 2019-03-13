2e39702bbf954641719f2b88302be3b9542f2c1e
Irina Gundersen <irinag@Irinas-MacBook-Pro.local>
2019-03-13 22:29:03 +0100

    heii

--- src/config/Øvelsesgruppe.java
+++ src/config/Øvelsesgruppe.java
@@ -1,1 +1,51 @@
-//Her havner alle filene som skriver og leser til databasen
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Time;
+
+public class Øvelsesgruppe extends Connecting{
+    private PreparedStatement rs;
+    private String beskrivelse;
+    private String øvelseNavn;
+
+
+
+
+    public Øvelsesgruppe(String beskrivelse) {
+
+        this.beskrivelse = beskrivelse;
+
+        registrerGruppe();
+
+    }
+    public void registrerGruppe() {
+        connect();
+
+        String query = "SELECT * FROM Ovelsesgruppe WHERE beskrivelse=(?)" ;
+        try {
+            rs = conn.prepareStatement("INSERT INTO Ovelsesgruppe(beskrivelse) VALUES ( (?))");
+            rs.setString(1, this.beskrivelse);
+
+            String duplicate = rs.getString("beskrivelse");
+
+            if (beskrivelse == duplicate){
+                return;
+            }
+
+            rs.execute();
+        }
+        catch (Exception e) {
+            System.out.println("db error during prepare of insert into treningsokt"+e);
+        }
+    }
+
+
+
+
+
+    public static void main(String[] args) {
+        Øvelsesgruppe og = new Øvelsesgruppe("Bein");
+
+    }
+
+}
